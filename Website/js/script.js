9dd0cad59a39441abd31975fb43341c80acb9d9d
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-07-11 13:34:51 +0200

    work link gedrag jquery OK, nu nog andere knoppen

--- Website/js/script.js
+++ Website/js/script.js
@@ -2,10 +2,23 @@
 
 $(document).ready(function() {
 
+    $(".st0").hover(function() {
+        $(".st0").css("fill", "#e7e7e7");
+        $(".st0").css("stroke", "#084E96");
+        $(".st0").css("stroke-width", "1.6px");
+        $("#nav_work_text").css("fill", "#084E96");
+    }, function() {
+        $(".st0").css("fill", "#084E96");
+        $(".st0").css("stroke", "#084E96");
+        $(".st0").css("stroke-width", "0px");
+        $("#nav_work_text").css("fill", "#e7e7e7");
+    });
+
     $("#nav_work_text").hover(function() {
         $(".st0").css("fill", "#e7e7e7");
         $(".st0").css("stroke", "#084E96");
         $(".st0").css("stroke-width", "1.6px");
+        $("#nav_work_text").css("fill", "#084E96");
     }, function() {
     });
 });
