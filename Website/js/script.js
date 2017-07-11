f9c2871368e1d1e7b25fab919e8d1e5dee881289
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-07-11 14:03:27 +0200

    about knoppen reageren al juist met jquery, enkel nog work en contact in desktop versie juist krijgen (stroke width)

--- Website/js/script.js
+++ Website/js/script.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function() {
 
+    /* work link */
+
     $(".st0").hover(function() {
         $(".st0").css("fill", "#e7e7e7");
         $(".st0").css("stroke", "#084E96");
@@ -21,4 +23,87 @@ $(document).ready(function() {
         $("#nav_work_text").css("fill", "#084E96");
     }, function() {
     });
+
+    $("#whiteline_work").hover(function() {
+        $(".st0").css("fill", "#e7e7e7");
+        $(".st0").css("stroke", "#084E96");
+        $(".st0").css("stroke-width", "1.6px");
+        $("#nav_work_text").css("fill", "#084E96");
+    }, function() {
+    });
+
+    /* contact link */
+
+    $(".st1").hover(function() {
+        $(".st1").css("fill", "#e7e7e7");
+        $(".st1").css("stroke", "#9b0408");
+        $(".st1").css("stroke-width", "1.6px");
+        $("#nav_contact_text").css("fill", "#9b0408");
+    }, function() {
+        $(".st1").css("fill", "#9b0408");
+        $(".st1").css("stroke", "#9b0408");
+        $(".st1").css("stroke-width", "0px");
+        $("#nav_contact_text").css("fill", "#e7e7e7");
+    });
+
+    $("#nav_contact_text").hover(function() {
+        $(".st1").css("fill", "#e7e7e7");
+        $(".st1").css("stroke", "#9b0408");
+        $(".st1").css("stroke-width", "1.6px");
+        $("#nav_contact_text").css("fill", "#9b0408");
+    }, function() {
+    });
+
+    $("#whiteline_contact").hover(function() {
+        $(".st1").css("fill", "#e7e7e7");
+        $(".st1").css("stroke", "#9b0408");
+        $(".st1").css("stroke-width", "1.6px");
+        $("#nav_contact_text").css("fill", "#9b0408");
+    }, function() {
+    });
+
+    /* about link 1 */
+
+    $(".st2").hover(function() {
+        $(".st2").css("fill", "#084E96");
+        $(".st2").css("stroke", "#e7e7e7");
+        $(".st2").css("stroke-width", "1.6px");
+        $("#nav_about1_text").css("fill", "#e7e7e7");
+    }, function() {
+        $(".st2").css("fill", "#e7e7e7");
+        $(".st2").css("stroke", "#084E96");
+        $(".st2").css("stroke-width", "2.6px");
+        $("#nav_about1_text").css("fill", "#084E96");
+    });
+
+    $("#nav_about1_text").hover(function() {
+        $(".st2").css("fill", "#084E96");
+        $(".st2").css("stroke", "#e7e7e7");
+        $(".st2").css("stroke-width", "1.6px");
+        $("#nav_about1_text").css("fill", "#e7e7e7");
+    }, function() {
+    });
+
+    /* about link 2 */
+
+    $(".st2b").hover(function() {
+        $(".st2b").css("fill", "#084E96");
+        $(".st2b").css("stroke", "#e7e7e7");
+        $(".st2b").css("stroke-width", "0px");
+        $("#nav_about2_text").css("fill", "#e7e7e7");
+    }, function() {
+        $(".st2b").css("fill", "#e7e7e7");
+        $(".st2b").css("stroke", "#084E96");
+        $(".st2b").css("stroke-width", "1.2px");
+        $("#nav_about2_text").css("fill", "#084E96");
+    });
+
+    $("#nav_about2_text").hover(function() {
+        $(".st2b").css("fill", "#084E96");
+        $(".st2b").css("stroke", "#e7e7e7");
+        $(".st2b").css("stroke-width", "0px");
+        $("#nav_about2_text").css("fill", "#e7e7e7");
+    }, function() {
+    });
+
 });
