52be69fdd8b616ff9fd9deee38b4291258086a55
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-07-26 19:48:35 +0200

    finale 404 under construction

--- Website/404.php
+++ Website/404.php
@@ -96,21 +96,19 @@
             <div class="cloud13"></div>
         </div>
 
-        <div class="fourohfour_flex">
-            <div class="fourohfour_part">
-                <div id="total_balloon">
-                    <div id="balloon"></div>
-                    <div id="fire"></div>
-                    <div id="dude"></div>
-                </div>
-            </div>
 
-            <div class="fourohfour_part">
-                <div id="text1" class="text">404</div>
-                <div id="text2" class="text">Deze reis is serieus stevig bezig met u nergens naartoe te brengen.</div>
-                <div id="terug_haar_home" class="text"><a href="index.php" >Begeef u terug naar de begane grond...</a></div>
-            </div>
+        <div id="total_balloon">
+            <div id="balloon"></div>
+            <div id="fire"></div>
+            <div id="dude"></div>
         </div>
+
+        <div id="textblock">
+            <div id="text1" class="text">404</div>
+            <div id="text2" class="text">Deze reis is serieus stevig bezig met u nergens naartoe te brengen.</div>
+            <div id="terug_haar_home" class="text"><a href="index.php" >Begeef u terug naar de begane grond...</a></div>
+        </div>
+
 
     </section>
 
